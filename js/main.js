eb30e2a98fca335f16ab96a21c19a29ba1ecdf13
Sryma <rurahgst@gmail.com>
2017-07-30 17:12:09 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
-  $('.carausel').slick({
+          
+ 
+ $(document).ready(function(){
+  $('.carousel').slick({
     autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-     focusOnSelect: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    draggable: false,
     slidesToShow: 1,
-  slidesToScroll: 1,
-  cssEase: 'linear'
+    slidesToScroll: 1
+  });
+
+
+ var scrollDistance = 0;
+  $(window).scroll(function(){
+    var topOfPage = $(this).scrollTop();
+    if (topOfPage - scrollDistance > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollDistance = topOfPage;
+    } else if (scrollDistance - topOfPage > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollDistance = topOfPage;
+    }
   });
 });
-              
- 
- 
+
